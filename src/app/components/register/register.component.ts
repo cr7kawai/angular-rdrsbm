f0ac9363da89c06b65acc7f60ea4074a760c6a11
cr7kawai <70409607+cr7kawai@users.noreply.github.com>
2023-02-01 21:07:54 -0600

    4 files modified

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  email:string; //Captura de email
+  password:string;  //Captura de password
+  confirmPassword:string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { //Inicializar los dos atributos
+    this.email = '';
+    this.password = ''
+    this.confirmPassword = '';
   }
 
+  register() {  //Función para evento clic
+    console.log('Email: ' + this.email);
+    console.log('Password: ' + this.password);
+    console.log('Confirm Password: ' + this.confirmPassword);
+  }
 }
+
